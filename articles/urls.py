bd01c8f9fbba7cac2017141b00a497dbdafd95d5
naerumii <github>
2022-12-09 09:33:49 +0900

    [수정] 축제 관련 urls.py 수정

--- articles/urls.py
+++ articles/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from articles import views
 
 urlpatterns = [
-    path('festival/', views.RecommendView.as_view(), name='recommend_view'),
-    path('check/', views.CheckView.as_view(), name='check_view'),
-    path('filter/', views.OptionView.as_view(), name='filter_view'),
+    path('recommend/', views.RecommendView.as_view(), name='recommend_view'),
+    path('festival/', views.FestivalListView.as_view(), name='festival_list_view'),
+    path('festival/filter/', views.FestivalFilterView.as_view(), name='festival_filter_view'),
     path('review/', views.ReviewView.as_view(), name='review_view'),
     path('review/<int:review_id>/', views.ReviewDetailView.as_view(), name='review_detail_view'),
     path('review/<int:review_id>/comment/', views.ReviewCommentView.as_view(), name='reveiw_comment_view'),
