3a37a6ccab086bd8b2fb9acbdec0d243812bb0b2
naerumii <github>
2022-12-13 10:55:26 +0900

    [수정] 리뷰 serializer 수정

--- articles/serializers.py
+++ articles/serializers.py
@@ -21,18 +21,6 @@ class FestivalSerializer(serializers.ModelSerializer):
         model = Festival_Article
         fields = ("pk", "festival_title", "festival_desc", "festival_image", "festival_region", "festival_cost", "festival_address", "festival_start", "festival_end", "bookmarks")
                 
-# 리뷰 리스트 serial
-class ReviewSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Review
-        fields = '__all__'
-
-# 리뷰 작성, 수정 serial
-class ReviewCreateSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Review
-        fields = ("review_title", "review_desc")
-
 # 리뷰 댓글 리스트 serial
 class ReviewCommentSerializer(serializers.ModelSerializer):
     review_user = serializers.SerializerMethodField()
@@ -43,13 +31,30 @@ class ReviewCommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review_Comment
         fields = '__all__'
-
+        
 # 리뷰 댓글 작성 serial
 class ReviewCommentCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review_Comment
         fields= ("review_comment",)
+          
+# 리뷰 작성, 수정 serial
+class ReviewCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = ("review_title", "review_desc", "image")
 
+# 리뷰 리스트 serial
+class ReviewSerializer(serializers.ModelSerializer):
+    review_author = serializers.SerializerMethodField()
+    review_comment = ReviewCommentSerializer(many=True)
+    
+    def get_review_author(self, obj):
+        return obj.review_author.user_nickname
+    
+    class Meta:
+        model = Review
+        fields = '__all__'
 
 #모집게시글 생성/수정 serial
 class JoinCreateSerializer(serializers.ModelSerializer):
