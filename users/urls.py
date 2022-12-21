dbf5e9afa6a420b77ae04e5f52d028ccf65f7a4b
yoojun <leehk77789@gmail.com>
2022-12-21 11:32:32 +0900

    [추가] profileview url추가

--- users/urls.py
+++ users/urls.py
@@ -5,6 +5,7 @@ from rest_framework_simplejwt.views import (
 )
 
 urlpatterns = [
+    path('', views.ProfileView.as_view(), name='profile_view'), 
     path('signup/', views.UserView.as_view(), name='user_view'),
     path('api/token/', views.CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
